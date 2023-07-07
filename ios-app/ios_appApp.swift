afee8b8f036a78583eab1050b2922a9409d82424
bobikenobi12 <borislav1207@gmail.com>
2023-07-07 15:10:49 +0300

    Initial Commit

---
+++ ios-app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios-app
+//
+//  Created by Borislav Borisov on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
